b945bd93c4b1ae3a99792a166991e95520667569
Bilise-Getachew <getachewbilise@gmail.com>
2026-01-31 16:38:02 +0300

    adding method to delete expenses by ID

--- src/main/java/com/gym/dao/impl/ExpenseDAOImpl.java
+++ src/main/java/com/gym/dao/impl/ExpenseDAOImpl.java
@@ -191,4 +191,17 @@ public class ExpenseDAOImpl implements ExpenseDAO{
         return expense;
     }
 
+    @Override
+    public boolean delete(int id) {
+        try (Connection conn = DatabaseUtil.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(DELETE_SQL)) {
+
+            stmt.setInt(1, id);
+            int affectedRows = stmt.executeUpdate();
+            return affectedRows > 0;
+        } catch (SQLException e) {
+            throw new RuntimeException("Error deleting expense", e);
+        }
+    }
+
 }
