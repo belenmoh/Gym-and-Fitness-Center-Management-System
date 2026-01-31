219e22618bbb3f03a3376b166de765256bc59a41
ametkemal2005-debug <ametkemal2005@gmail.com>
2026-01-31 15:33:38 +0300

    final commit

--- src/main/java/com/gym/dao/UserDAO.java
+++ src/main/java/com/gym/dao/UserDAO.java
@@ -1,15 +1,4 @@
-package com.gym.dao;
+package PACKAGE_NAME;
 
-import com.gym.model.User;
-import java.util.List;
-import java.util.Optional;
-
-public interface UserDAO {
-    User save(User user);
-    Optional<User> findById(int id);
-    Optional<User> findByUsername(String username);
-    List<User> findAll();
-    User update(User user);
-    boolean delete(int id);
-    boolean existsByUsername(String username);
+public class UserDAO {
 }
