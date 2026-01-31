d7b3fbe683a8750df6fb01b5bd250ce231939ae8
alazar-ship <Abaynehalazar@gmail.com>
2026-01-31 12:21:46 +0300

    updated annual membership methods

--- src/main/java/com/gym/model/AnnualMembership.java
+++ src/main/java/com/gym/model/AnnualMembership.java
@@ -11,4 +11,18 @@ public class AnnualMembership extends Membership{
     public AnnualMembership(int id, String name, double price) {
         super(id, name, price, 12);
     }
+    @Override
+    public double calculateDiscount() {
+        return getPrice() * DISCOUNT_RATE;
+    }
+
+    @Override
+    public String getBenefits() {
+        return "Full gym access, locker room, all equipment, 1 free personal training session per month";
+    }
+
+    @Override
+    public String toString() {
+        return "AnnualMembership{} " + super.toString();
+    }
 }
