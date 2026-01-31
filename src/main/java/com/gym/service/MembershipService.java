7c465f6fbfa7d899f10928fb1f8417faaefd4531
Bilise-Getachew <getachewbilise@gmail.com>
2026-01-31 17:38:56 +0300

    fix package and imports

--- src/main/java/com/gym/service/MembershipService.java
+++ src/main/java/com/gym/service/MembershipService.java
@@ -1,2 +1,16 @@
+package com.gym.service;
+
+import com.gym.dao.MemberDAO;
+import com.gym.dao.UserDAO;
+import com.gym.model.Member;
+import com.gym.model.Membership;
+import com.gym.model.MonthlyMembership;
+import com.gym.model.AnnualMembership;
+import com.gym.model.VIPMembership;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
 public class MembershipService {
 }
