4a4d92380a763391c8ccc14c37262cf05126d68e
Bilise-Getachew <getachewbilise@gmail.com>
2026-01-31 17:17:26 +0300

    fixing package and imports

--- src/main/java/com/gym/dao/impl/UserDAOImpl.java
+++ src/main/java/com/gym/dao/impl/UserDAOImpl.java
@@ -1,4 +1,18 @@
-package impl;
+package com.gym.dao.impl;
 
-public class UserDAOImpl {
+import com.gym.dao.UserDAO;
+import com.gym.model.User;
+import com.gym.model.UserRole;
+import com.gym.model.Member;
+import com.gym.model.Receptionist;
+import com.gym.model.Admin;
+import com.gym.util.DatabaseUtil;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class UserDAOImpl implements UserDAO{
+
 }
