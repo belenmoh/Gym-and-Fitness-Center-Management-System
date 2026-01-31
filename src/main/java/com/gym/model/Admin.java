b7b78c1d185db9c6913e9fe3ce587268b140b214
alazar-ship <Abaynehalazar@gmail.com>
2026-01-31 12:08:58 +0300

    constructed admin class

--- src/main/java/com/gym/model/Admin.java
+++ src/main/java/com/gym/model/Admin.java
@@ -1,5 +1,15 @@
 package com.gym.model;
 
 public class Admin extends User{
+    private int adminId;
 
+    public Admin() {
+        super();
+        setRole(UserRole.ADMIN);
+    }
+
+    public Admin(int id, String name, String username, String password, int adminId) {
+        super(id, name, username, password, UserRole.ADMIN);
+        this.adminId = adminId;
+    }
 }
