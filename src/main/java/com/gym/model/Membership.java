c889b527f822ed1335d5d30d7c484e6f4d01508a
alazar-ship <Abaynehalazar@gmail.com>
2026-01-31 12:12:24 +0300

    constructed membership class

--- src/main/java/com/gym/model/Membership.java
+++ src/main/java/com/gym/model/Membership.java
@@ -1,4 +1,17 @@
 package com.gym.model;
 
 public abstract class Membership {
+    private int id;
+    private String name;
+    private double price;
+    private int durationMonths;
+
+    public Membership() {}
+
+    public Membership(int id, String name, double price, int durationMonths) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.durationMonths = durationMonths;
+    }
 }
