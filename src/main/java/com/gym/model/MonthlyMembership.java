e6ab499b3b8f2d89d1cc0b2137d6b3ba6143d5ba
alazar-ship <Abaynehalazar@gmail.com>
2026-01-31 12:17:43 +0300

    constructed monthly membership class

--- src/main/java/com/gym/model/MonthlyMembership.java
+++ src/main/java/com/gym/model/MonthlyMembership.java
@@ -1,4 +1,14 @@
 package com.gym.model;
 
 public class MonthlyMembership extends Membership{
+    private static final double DISCOUNT_RATE = 0.0;
+
+    public MonthlyMembership() {
+        super();
+        setDurationMonths(1);
+    }
+
+    public MonthlyMembership(int id, String name, double price) {
+        super(id, name, price, 1);
+    }
 }
