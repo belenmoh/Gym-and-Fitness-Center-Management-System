504a7972f59cadb237a4b27e6796ad23e56ce0a2
alazar-ship <Abaynehalazar@gmail.com>
2026-01-31 12:06:07 +0300

    constructed user class

--- src/main/java/com/gym/model/User.java
+++ src/main/java/com/gym/model/User.java
@@ -1,4 +1,19 @@
 package com.gym.model;
 
 public abstract class User {
+    private int id;
+    private String name;
+    private String username;
+    private String password;
+    private UserRole role;
+
+    public User() {}
+
+    public User(int id, String name, String username, String password, UserRole role) {
+        this.id = id;
+        this.name = name;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
 }
