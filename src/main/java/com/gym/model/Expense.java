a5a59a4e9b4de68cb3b909867ba73b6dc72682d8
alazar-ship <Abaynehalazar@gmail.com>
2026-01-31 12:28:46 +0300

    added expense methods

--- src/main/java/com/gym/model/Expense.java
+++ src/main/java/com/gym/model/Expense.java
@@ -18,4 +18,44 @@ public class Expense {
         this.date = date;
         this.category = category;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public ExpenseCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(ExpenseCategory category) {
+        this.category = category;
+    }
 }
