082ab1ea075cfd111629389ebd5d6cc34e476d11
ametkemal2005-debug <ametkemal2005@gmail.com>
2026-01-31 14:32:14 +0300

    fix package and import

--- src/main/java/com/gym/dao/BookingDAO.java
+++ src/main/java/com/gym/dao/BookingDAO.java
@@ -1,2 +1,11 @@
-public class BookingDAO {
+package com.gym.dao;
+
+import com.gym.model.Booking;
+import com.gym.model.BookingStatus;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+public interface BookingDAO {
+
 }
