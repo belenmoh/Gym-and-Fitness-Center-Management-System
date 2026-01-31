a416b8de49940c50487cc837d2d70f7359d8b8e0
alazar-ship <Abaynehalazar@gmail.com>
2026-01-31 12:42:47 +0300

    constructed payment class

--- src/main/java/com/gym/model/Payment.java
+++ src/main/java/com/gym/model/Payment.java
@@ -3,4 +3,19 @@ package com.gym.model;
 import java.time.LocalDate;
 
 public class Payment {
+    private int id;
+    private int memberId;
+    private double amount;
+    private LocalDate date;
+    private PaymentType type;
+
+    public Payment() {}
+
+    public Payment(int id, int memberId, double amount, LocalDate date, PaymentType type) {
+        this.id = id;
+        this.memberId = memberId;
+        this.amount = amount;
+        this.date = date;
+        this.type = type;
+    }
 }
