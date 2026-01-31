effe3759ee4820a3c8775480133e8f937bc25ba2
ametkemal2005-debug <ametkemal2005@gmail.com>
2026-01-31 14:16:50 +0300

    add SQLite database URL and initialization flag to DatabaseUtil

--- src/main/java/com/gym/util/com/gym/util/DatabaseUtil.java
+++ src/main/java/com/gym/util/com/gym/util/DatabaseUtil.java
@@ -6,4 +6,6 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DatabaseUtil {
+    private static final String DB_URL = "jdbc:sqlite:gym_database.db";
+    private static boolean initialized = false;
 }
