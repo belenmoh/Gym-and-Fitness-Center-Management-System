f846a95c16d3dcc237985fcc7ddd04a090b5c930
alazar-ship <Abaynehalazar@gmail.com>
2026-01-31 12:26:15 +0300

    constructed expense class

--- src/main/java/com/gym/model/Expense.java
+++ src/main/java/com/gym/model/Expense.java
@@ -3,4 +3,19 @@ package com.gym.model;
 import java.time.LocalDate;
 
 public class Expense {
+    private int id;
+    private String description;
+    private double amount;
+    private LocalDate date;
+    private ExpenseCategory category;
+
+    public Expense() {}
+
+    public Expense(int id, String description, double amount, LocalDate date, ExpenseCategory category) {
+        this.id = id;
+        this.description = description;
+        this.amount = amount;
+        this.date = date;
+        this.category = category;
+    }
 }
