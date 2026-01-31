296d2eef94c664bce599e263f4ce1874b903da6c
alazar-ship <Abaynehalazar@gmail.com>
2026-01-31 12:07:21 +0300

    added user methods

--- src/main/java/com/gym/model/User.java
+++ src/main/java/com/gym/model/User.java
@@ -16,4 +16,44 @@ public abstract class User {
         this.password = password;
         this.role = role;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
 }
