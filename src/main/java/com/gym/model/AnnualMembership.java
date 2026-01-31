3672765a49d0828104afc6e9b9804d594da71865
alazar-ship <Abaynehalazar@gmail.com>
2026-01-31 12:21:03 +0300

    constructed annual membership class

--- src/main/java/com/gym/model/AnnualMembership.java
+++ src/main/java/com/gym/model/AnnualMembership.java
@@ -1,4 +1,14 @@
 package com.gym.model;
 
 public class AnnualMembership extends Membership{
+    private static final double DISCOUNT_RATE = 0.15;
+
+    public AnnualMembership() {
+        super();
+        setDurationMonths(12);
+    }
+
+    public AnnualMembership(int id, String name, double price) {
+        super(id, name, price, 12);
+    }
 }
