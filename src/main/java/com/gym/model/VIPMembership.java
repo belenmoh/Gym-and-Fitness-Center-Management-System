3b7742f234a5c890ecd1739a6248bc5dff127eab
alazar-ship <Abaynehalazar@gmail.com>
2026-01-31 12:50:03 +0300

    updated VIPMembership methods

--- src/main/java/com/gym/model/VIPMembership.java
+++ src/main/java/com/gym/model/VIPMembership.java
@@ -11,4 +11,19 @@ public class VIPMembership extends Membership{
     public VIPMembership(int id, String name, double price) {
         super(id, name, price, 12);
     }
+
+    @Override
+    public double calculateDiscount() {
+        return getPrice() * DISCOUNT_RATE;
+    }
+
+    @Override
+    public String getBenefits() {
+        return "All gym access, VIP locker room, premium equipment, unlimited personal training, spa access, nutrition consultation";
+    }
+
+    @Override
+    public String toString() {
+        return "VIPMembership{} " + super.toString();
+    }
 }
