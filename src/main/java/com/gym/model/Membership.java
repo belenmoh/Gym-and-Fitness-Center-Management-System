7989effd3c6b909773da72ca92a92cb5525d4b17
alazar-ship <Abaynehalazar@gmail.com>
2026-01-31 12:13:52 +0300

    added membership methods

--- src/main/java/com/gym/model/Membership.java
+++ src/main/java/com/gym/model/Membership.java
@@ -14,4 +14,40 @@ public abstract class Membership {
         this.price = price;
         this.durationMonths = durationMonths;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getDurationMonths() {
+        return durationMonths;
+    }
+
+    public void setDurationMonths(int durationMonths) {
+        this.durationMonths = durationMonths;
+    }
+
+    public abstract double calculateDiscount();
+
+    public abstract String getBenefits();
 }
