7feba3661ed53597008f39797ce33f5414888ac0
alazar-ship <Abaynehalazar@gmail.com>
2026-01-31 12:35:22 +0300

    constructed financial report class

--- src/main/java/com/gym/model/FinancialReport.java
+++ src/main/java/com/gym/model/FinancialReport.java
@@ -1,4 +1,19 @@
 package com.gym.model;
 
 public class FinancialReport {
+    private int month;
+    private int year;
+    private double totalIncome;
+    private double totalExpenses;
+    private double netCashFlow;
+
+    public FinancialReport() {}
+
+    public FinancialReport(int month, int year, double totalIncome, double totalExpenses) {
+        this.month = month;
+        this.year = year;
+        this.totalIncome = totalIncome;
+        this.totalExpenses = totalExpenses;
+        this.netCashFlow = totalIncome - totalExpenses;
+    }
 }
