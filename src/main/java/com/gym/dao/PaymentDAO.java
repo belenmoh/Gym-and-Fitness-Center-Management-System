65bbcb559ed77fe9fdb7e38c22863f0250425b03
ametkemal2005-debug <ametkemal2005@gmail.com>
2026-01-31 14:41:58 +0300

    fix package and imports

--- src/main/java/com/gym/dao/PaymentDAO.java
+++ src/main/java/com/gym/dao/PaymentDAO.java
@@ -1,2 +1,11 @@
+package com.gym.dao;
+
+import com.gym.model.Payment;
+import com.gym.model.PaymentType;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
 public class PaymentDAO {
+
 }
