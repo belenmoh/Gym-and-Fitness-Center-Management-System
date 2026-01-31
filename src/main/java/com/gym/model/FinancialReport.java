151fe4cde5ba186332703af903835ac8d63ba492
alazar-ship <Abaynehalazar@gmail.com>
2026-01-31 12:36:20 +0300

    added financial report methods

--- src/main/java/com/gym/model/FinancialReport.java
+++ src/main/java/com/gym/model/FinancialReport.java
@@ -16,4 +16,43 @@ public class FinancialReport {
         this.totalExpenses = totalExpenses;
         this.netCashFlow = totalIncome - totalExpenses;
     }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public double getTotalIncome() {
+        return totalIncome;
+    }
+
+    public void setTotalIncome(double totalIncome) {
+        this.totalIncome = totalIncome;
+        this.netCashFlow = this.totalIncome - this.totalExpenses;
+    }
+
+    public double getTotalExpenses() {
+        return totalExpenses;
+    }
+
+    public void setTotalExpenses(double totalExpenses) {
+        this.totalExpenses = totalExpenses;
+        this.netCashFlow = this.totalIncome - this.totalExpenses;
+    }
+
+    public double getNetCashFlow() {
+        return netCashFlow;
+    }
+
 }
