d4f191632f13e77dc83953da69c4d20d703c029d
alazar-ship <Abaynehalazar@gmail.com>
2026-01-31 12:23:01 +0300

    constucted booking class

--- src/main/java/com/gym/model/Booking.java
+++ src/main/java/com/gym/model/Booking.java
@@ -3,4 +3,21 @@ package com.gym.model;
 import java.time.LocalDateTime;
 
 public class Booking {
+    private int id;
+    private int memberId;
+    private String className;
+    private LocalDateTime bookingTime;
+    private LocalDateTime classTime;
+    private BookingStatus status;
+
+    public Booking() {}
+
+    public Booking(int id, int memberId, String className, LocalDateTime bookingTime, LocalDateTime classTime) {
+        this.id = id;
+        this.memberId = memberId;
+        this.className = className;
+        this.bookingTime = bookingTime;
+        this.classTime = classTime;
+        this.status = BookingStatus.BOOKED;
+    }
 }
