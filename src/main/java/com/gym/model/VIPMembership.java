f15628350e7b7f8f4e20817199572068bcfc18f1
alazar-ship <Abaynehalazar@gmail.com>
2026-01-31 12:49:19 +0300

    constructed VIPMembership class

--- src/main/java/com/gym/model/VIPMembership.java
+++ src/main/java/com/gym/model/VIPMembership.java
@@ -1,4 +1,14 @@
 package com.gym.model;
 
 public class VIPMembership extends Membership{
+    private static final double DISCOUNT_RATE = 0.25;
+
+    public VIPMembership() {
+        super();
+        setDurationMonths(12);
+    }
+
+    public VIPMembership(int id, String name, double price) {
+        super(id, name, price, 12);
+    }
 }
