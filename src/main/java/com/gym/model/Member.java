5a31740dfb992aa86bff6e448baa40d6886d254f
alazar-ship <Abaynehalazar@gmail.com>
2026-01-31 12:38:35 +0300

    constructed member class

--- src/main/java/com/gym/model/Member.java
+++ src/main/java/com/gym/model/Member.java
@@ -2,5 +2,23 @@ package com.gym.model;
 
 import java.time.LocalDate;
 
-public class Member {
+public class Member extends User{
+    private int memberId;
+    private Membership membership;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    public Member() {
+        super();
+        setRole(UserRole.MEMBER);
+    }
+
+    public Member(int id, String name, String username, String password,
+                  int memberId, Membership membership, LocalDate startDate, LocalDate endDate) {
+        super(id, name, username, password, UserRole.MEMBER);
+        this.memberId = memberId;
+        this.membership = membership;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
 }
