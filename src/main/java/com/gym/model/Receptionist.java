fdbf4bae1bd16af71bde9a1366aff5168c3c4431
alazar-ship <Abaynehalazar@gmail.com>
2026-01-31 12:46:07 +0300

    constructed receptionist methods

--- src/main/java/com/gym/model/Receptionist.java
+++ src/main/java/com/gym/model/Receptionist.java
@@ -1,4 +1,15 @@
 package com.gym.model;
 
 public class Receptionist extends User{
+    private int employeeId;
+
+    public Receptionist() {
+        super();
+        setRole(UserRole.RECEPTIONIST);
+    }
+
+    public Receptionist(int id, String name, String username, String password, int employeeId) {
+        super(id, name, username, password, UserRole.RECEPTIONIST);
+        this.employeeId = employeeId;
+    }
 }
