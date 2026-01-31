08cb3d8c512b77fd990ef12fdb6ae3338dfeaf00
ametkemal2005-debug <ametkemal2005@gmail.com>
2026-01-31 14:46:05 +0300

    fix package and imports

--- src/main/java/com/gym/dao/UserDAO.java
+++ src/main/java/com/gym/dao/UserDAO.java
@@ -4,5 +4,12 @@ import com.gym.model.User;
 import java.util.List;
 import java.util.Optional;
 
-public class UserDAO {
+public interface UserDAO {
+    User save(User user);
+    Optional<User> findById(int id);
+    Optional<User> findByUsername(String username);
+    List<User> findAll();
+    User update(User user);
+    boolean delete(int id);
+    boolean existsByUsername(String username);
 }
