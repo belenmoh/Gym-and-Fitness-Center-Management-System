9d47e2349eec2fe57604f1ed20759fd0d8bd82fa
ametkemal2005-debug <ametkemal2005@gmail.com>
2026-01-31 14:36:43 +0300

    fix package and imports

--- src/main/java/com/gym/dao/ExpenseDAO.java
+++ src/main/java/com/gym/dao/ExpenseDAO.java
@@ -1,2 +1,10 @@
-public class ExpenseDAO {
+package com.gym.dao;
+
+import com.gym.model.Expense;
+import com.gym.model.ExpenseCategory;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+public interface ExpenseDAO {
 }
