89ae3abc59611e75683161b4df9289260d33fa89
belenmoh <belenbubumoh@gmail.com>
2026-02-02 00:22:34 +0300

    updated receptionist methods

--- src/main/java/com/gym/model/Receptionist.java
+++ src/main/java/com/gym/model/Receptionist.java
@@ -7,16 +7,13 @@ public class Receptionist extends User{
         super();
         setRole(UserRole.RECEPTIONIST);
     }
-
     public Receptionist(int id, String name, String username, String password, int employeeId) {
         super(id, name, username, password, UserRole.RECEPTIONIST);
         this.employeeId = employeeId;
     }
-
     public int getEmployeeId() {
         return employeeId;
     }
-
     public void setEmployeeId(int employeeId) {
         this.employeeId = employeeId;
     }
