73f2385b9337063547699d697854972e0c28c160
belenmoh <belenbubumoh@gmail.com>
2026-02-02 00:29:59 +0300

    created gym application class extending Application

---
+++ src/main/java/com/gym/GymApplication.java
@@ -0,0 +1,20 @@
+package com.gym;
+
+import com.gym.dao.UserDAO;
+import com.gym.dao.impl.UserDAOImpl;
+import com.gym.model.Admin;
+import com.gym.model.Member;
+import com.gym.model.Receptionist;
+import com.gym.model.UserRole;
+import com.gym.util.DatabaseUtil;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class GymApplication extends Application {
+
+}
