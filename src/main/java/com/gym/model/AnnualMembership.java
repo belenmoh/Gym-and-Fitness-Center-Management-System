b30c2441d0f4312f69d4c1b0dc966f56f07d36d7
belenmoh <belenbubumoh@gmail.com>
2026-02-02 00:14:11 +0300

    updated annual membership methods

--- src/main/java/com/gym/model/AnnualMembership.java
+++ src/main/java/com/gym/model/AnnualMembership.java
@@ -1,13 +1,12 @@
 package com.gym.model;
 
 public class AnnualMembership extends Membership{
-    private static final double DISCOUNT_RATE = 0.15;
 
+    private static final double DISCOUNT_RATE = 0.15;
     public AnnualMembership() {
         super();
         setDurationMonths(12);
     }
-
     public AnnualMembership(int id, String name, double price) {
         super(id, name, price, 12);
     }
@@ -20,7 +19,6 @@ public class AnnualMembership extends Membership{
     public String getBenefits() {
         return "Full gym access, locker room, all equipment, 1 free personal training session per month";
     }
-
     @Override
     public String toString() {
         return "AnnualMembership{} " + super.toString();
