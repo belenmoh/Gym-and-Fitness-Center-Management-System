ed32cb14a33b590142d09d1693061584354743c9
belenmoh <belenbubumoh@gmail.com>
2026-02-02 00:06:14 +0300

    updated user DAO implementation

--- src/main/java/com/gym/dao/impl/UserDAOImpl.java
+++ src/main/java/com/gym/dao/impl/UserDAOImpl.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 public class UserDAOImpl implements UserDAO {
+
     private static final String INSERT_SQL =
             "INSERT INTO users (name, username, password, role) VALUES (?, ?, ?, ?)";
     private static final String SELECT_BY_ID_SQL =
