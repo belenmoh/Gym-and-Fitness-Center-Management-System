329fa0f046f3f4c30eb85f8ea823a852ee5427e4
belenmoh <belenbubumoh@gmail.com>
2026-02-02 00:23:19 +0300

    updated user methods

--- src/main/java/com/gym/model/User.java
+++ src/main/java/com/gym/model/User.java
@@ -8,7 +8,6 @@ public abstract class User {
     private UserRole role;
 
     public User() {}
-
     public User(int id, String name, String username, String password, UserRole role) {
         this.id = id;
         this.name = name;
@@ -16,43 +15,33 @@ public abstract class User {
         this.password = password;
         this.role = role;
     }
-
     public int getId() {
         return id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getUsername() {
         return username;
     }
-
     public void setUsername(String username) {
         this.username = username;
     }
-
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
-
     public UserRole getRole() {
         return role;
     }
-
     public void setRole(UserRole role) {
         this.role = role;
     }
