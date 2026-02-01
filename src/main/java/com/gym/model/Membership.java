60aaf4bd4e8f71e108b6ee12199959a1f14d1d21
belenmoh <belenbubumoh@gmail.com>
2026-02-02 00:19:44 +0300

    updated membership methods

--- src/main/java/com/gym/model/Membership.java
+++ src/main/java/com/gym/model/Membership.java
@@ -7,7 +7,6 @@ public abstract class Membership {
     private int durationMonths;
 
     public Membership() {}
-
     public Membership(int id, String name, double price, int durationMonths) {
         this.id = id;
         this.name = name;
@@ -18,37 +17,29 @@ public abstract class Membership {
     public int getId() {
         return id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public double getPrice() {
         return price;
     }
-
     public void setPrice(double price) {
         this.price = price;
     }
-
     public int getDurationMonths() {
         return durationMonths;
     }
-
     public void setDurationMonths(int durationMonths) {
         this.durationMonths = durationMonths;
     }
 
     public abstract double calculateDiscount();
-
     public abstract String getBenefits();
     @Override
     public String toString() {
