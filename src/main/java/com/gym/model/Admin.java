0773c6e830062379498f64b2368aa215952ff593
belenmoh <belenbubumoh@gmail.com>
2026-02-02 00:12:35 +0300

    updated admin methods

--- src/main/java/com/gym/model/Admin.java
+++ src/main/java/com/gym/model/Admin.java
@@ -7,16 +7,13 @@ public class Admin extends User{
         super();
         setRole(UserRole.ADMIN);
     }
-
     public Admin(int id, String name, String username, String password, int adminId) {
         super(id, name, username, password, UserRole.ADMIN);
         this.adminId = adminId;
     }
-
     public int getAdminId() {
         return adminId;
     }
-
     public void setAdminId(int adminId) {
         this.adminId = adminId;
     }
