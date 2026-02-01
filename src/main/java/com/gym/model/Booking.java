ef5b1a6f1a4f61bfd7c3716bdace6c43765818a3
belenmoh <belenbubumoh@gmail.com>
2026-02-02 00:15:29 +0300

    updated booking methods

--- src/main/java/com/gym/model/Booking.java
+++ src/main/java/com/gym/model/Booking.java
@@ -11,7 +11,6 @@ public class Booking {
     private BookingStatus status;
 
     public Booking() {}
-
     public Booking(int id, int memberId, String className, LocalDateTime bookingTime, LocalDateTime classTime) {
         this.id = id;
         this.memberId = memberId;
@@ -24,47 +23,36 @@ public class Booking {
     public int getId() {
         return id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
-
     public int getMemberId() {
         return memberId;
     }
-
     public void setMemberId(int memberId) {
         this.memberId = memberId;
     }
-
     public String getClassName() {
         return className;
     }
-
     public void setClassName(String className) {
         this.className = className;
     }
-
     public LocalDateTime getBookingTime() {
         return bookingTime;
     }
-
     public void setBookingTime(LocalDateTime bookingTime) {
         this.bookingTime = bookingTime;
     }
-
     public LocalDateTime getClassTime() {
         return classTime;
     }
-
     public void setClassTime(LocalDateTime classTime) {
         this.classTime = classTime;
     }
-
     public BookingStatus getStatus() {
         return status;
     }
-
     public void setStatus(BookingStatus status) {
         this.status = status;
     }
