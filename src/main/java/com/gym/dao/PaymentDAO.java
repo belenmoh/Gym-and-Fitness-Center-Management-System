84feca40947420ad73d3bee8c544ac2df707aa01
belenmoh <belenbubumoh@gmail.com>
2026-02-02 00:10:50 +0300

    updated payment DAO

--- src/main/java/com/gym/dao/PaymentDAO.java
+++ src/main/java/com/gym/dao/PaymentDAO.java
@@ -5,7 +5,6 @@ import com.gym.model.PaymentType;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
-
 public interface PaymentDAO {
     Payment save(Payment payment);
     Optional<Payment> findById(int id);
